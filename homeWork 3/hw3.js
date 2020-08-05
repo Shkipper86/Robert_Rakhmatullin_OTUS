413668e6e08f4f120e4cd7b24e88faa78b6fa404
Shkipper86 <Shkipper.r@yandex.ru>
2020-08-06 00:27:41 +0300

    Create hw3.js

---
+++ homeWork 3/hw3.js
@@ -0,0 +1,33 @@
+const data = {
+    "HP": {
+      "3PAR": {},
+      "ProLiant": {}
+    },
+    "Lenovo": {
+      "Blade Switch": {
+        "BNT Layer 2/3 Copper GbE": {},
+        "IBM Flex System EN2092": {}
+      },
+      "Server": {
+        "ThinkCentre A57": {},
+        "ThinkSystem SR650": {}
+      }
+    }
+  };
+  function myTree(container, data) {    
+    container.appendChild(myLeaf(data));              
+  }
+  function myLeaf(data) { 
+    let ul = document.createElement('ul');  
+    for(let key in data) {
+       let li = document.createElement('li');
+          li.innerHTML = key;
+          let childrenUl = myLeaf(data[key]); 
+          if (childrenUl) li.appendChild(childrenUl);
+          ul.appendChild(li);   
+      }
+      return ul;      
+  }    
+  tree.attachShadow({mode: 'open'}); 
+  const container = document.getElementById('tree');
+  tree.shadowRoot.innerHTML=  myTree(container, data);
