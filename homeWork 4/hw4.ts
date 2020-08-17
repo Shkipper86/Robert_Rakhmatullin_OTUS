b6fd52afe0f032cda98779ff48e32e188a9e487b
Shkipper86 <Shkipper.r@yandex.ru>
2020-08-17 22:15:45 +0300

    Create hw4.ts

---
+++ homeWork 4/hw4.ts
@@ -0,0 +1,18 @@
+function PreSaveAction(): boolean {
+  interface URLString {
+    URLP: String;
+    tit: String;
+  }
+    let test:string = $('nobr:contains("Договор / Заказ")').closest('tr').find('.ms-rtestate-write').html();
+    let arr:string []= test.split('');
+    let s:number = arr.length;
+    if (s<=8){
+      let input: string = prompt('Введите ссылку на договор в формате: название договора#ссылка');
+      let [URLP, tit] = input.split('#');
+      $('nobr:contains("Договор / Заказ")').closest('tr').find('.ms-rtestate-write').html(`<a href="${URLP}" target="_blank">${tit}</a>`);
+      return true;
+    }else{
+      return true;
+    }
+} 
+
