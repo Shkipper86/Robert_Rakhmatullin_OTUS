c05d4684638291bef1963c3049c28f718c2adac5
Shkipper86 <Shkipper.r@yandex.ru>
2020-08-18 16:09:52 +0300

    Update hw4.js

--- homeWork 4/hw4.js
+++ homeWork 4/hw4.js
@@ -4,11 +4,15 @@ function PreSaveAction() {
     var s = arr.length;
     if (s <= 8) {
         var input = prompt('Введите ссылку на договор в формате: название договора#ссылка');
-        var _a = input.split('#'), URLP = _a[0], tit = _a[1];
-        $('nobr:contains("Договор / Заказ")').closest('tr').find('.ms-rtestate-write').html("<a href=\"" + URLP + "\" target=\"_blank\">" + tit + "</a>");
-        return true;
+        inputString();
     }
-    else {
-        return true;
+    return true;
+}
+function inputString() {
+    var input = prompt('Введите ссылку на договор в формате: название договора#ссылка');
+    var _a = input.split('#'), tit = _a[0], URLP = _a[1], rest = _a.slice(2);
+    if (URLP == null) {
+        PreSaveAction();
     }
+    $('nobr:contains("Договор / Заказ")').closest('tr').find('.ms-rtestate-write').html("<a href=\"" + URLP + "\" target=\"_blank\">" + tit + "</a>");
 }
