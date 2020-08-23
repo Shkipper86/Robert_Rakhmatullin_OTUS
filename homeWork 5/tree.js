6264c2b90c0a9bcc750d3ea9e933ebbe82328090
Shkipper86 <Shkipper.r@yandex.ru>
2020-08-23 23:49:00 +0300

    Update tree.js

--- homeWork 5/tree.js
+++ homeWork 5/tree.js
@@ -1,8 +1,29 @@
-const testFolder = './newfolder/';
 const fs = require('fs');
+let path = process.argv[2];;
+let pathString = path;
+let tempPath;
+function recu(){
+  fs.readdir(pathString, (err, files) =>{ 
+    try{
+    files.forEach(file=> {
+      tempPath=pathString;      
+      fs.stat(`${pathString}/${file}`, (err, stats)=>{
+        if (err){
 
-fs.readdir(testFolder, (err, files) => {
-  files.forEach(file => {
-    console.log(file);
-  });
-});
+        }
+        else if (stats.isDirectory()){ 
+          pathString=`${pathString}/${file}`; 
+          console.log(pathString);
+          recu();
+        }else{
+          console.log(`${tempPath}/${file}`);
+        }
+      })     
+    })
+  }
+  catch (err){
+
+  }  
+})
+}
+recu();
