a98729ad28709d84b976b9701b8cea4c2743d4ba
Shkipper86 <Shkipper.r@yandex.ru>
2020-08-27 22:57:03 +0300

    Update hw2.js

--- homeWork 2/hw2.js
+++ homeWork 2/hw2.js
@@ -1,16 +1,26 @@
-document.addEventListener('click', function(e){
-  const t = e.target.TagName;
-  const sel = "search"
-  e.target.id = sel; 
-  getPath(sel);
-});
-function getPath(sel) { 
-console.log(document.querySelector(`#${sel}`));
-console.log(document.querySelectorAll(`#${sel}`));
-return document.querySelectorAll(`#${sel}`).length;
-}
-const testModule = (sel)=> {
-  document.querySelectorAll('li')[0].id=sel;
-  return document.querySelectorAll(`#${sel}`).length;
-   }
-module.exports = testModule;
+    document.addEventListener('click', function(e){
+        let path = e.path;     
+        let uniqSelector=path[0].nodeName;
+        let i=1;
+        try {
+            function check (){
+                let tempSelector = path[i].nodeName;
+                if (tempSelector != undefined){
+                    uniqSelector=`${uniqSelector}/${tempSelector}`;                    
+                    i++;
+                    check();
+                }
+                }
+               check();
+               const arr = uniqSelector.split('/');
+               uniqSelector='';
+               
+               for (let j=i-3;j>-1;j--){
+                   uniqSelector=`${uniqSelector} ${arr[j]}`;
+               }  
+               console.log(document.querySelector(uniqSelector));
+               console.log(document.querySelectorAll(`${uniqSelector}:first-child`));
+        }catch(err){
+            console.log('error');           
+        }
+        }) 
