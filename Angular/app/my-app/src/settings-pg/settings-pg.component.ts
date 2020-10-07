1b3e613fa67acc7ea2450a9eb1a9b9086414f6d3
Shkipper86 <Shkipper.r@yandex.ru>
2020-10-07 22:41:55 +0300

    Create settings-pg.component.ts

---
+++ Angular/app/my-app/src/settings-pg/settings-pg.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-settings-pg',
+  templateUrl: './settings-pg.component.html',
+  styleUrls: ['./settings-pg.component.css']
+})
+export class SettingsPgComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
