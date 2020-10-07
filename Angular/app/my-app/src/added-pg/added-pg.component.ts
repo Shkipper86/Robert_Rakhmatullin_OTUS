20db120fd1b8f5e7dc49d41f456a1da3f7c99b06
Shkipper86 <Shkipper.r@yandex.ru>
2020-10-07 22:56:05 +0300

    Create added-pg.component.ts

---
+++ Angular/app/my-app/src/added-pg/added-pg.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-added-pg',
+  templateUrl: './added-pg.component.html',
+  styleUrls: ['./added-pg.component.css']
+})
+export class AddedPgComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
