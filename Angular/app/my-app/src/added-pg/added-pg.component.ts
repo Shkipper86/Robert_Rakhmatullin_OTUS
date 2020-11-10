388f92660ae92a1ab6c93239316fc74bde9e67d1
Shkipper86 <Shkipper.r@yandex.ru>
2020-11-10 22:17:28 +0300

    Update added-pg.component.ts

--- Angular/app/my-app/src/added-pg/added-pg.component.ts
+++ Angular/app/my-app/src/added-pg/added-pg.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./added-pg.component.css']
 })
 export class AddedPgComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit(): void {
+    let output: string = '';
+    for (let i: number = 0; i < localStorage.length-3; i++){
+      output=`${output}\n${localStorage.key(i)}: ${localStorage.getItem(localStorage.key(i))}\n`;
+    }
+    document.querySelector("textarea").value=output;
   }
 
 }
