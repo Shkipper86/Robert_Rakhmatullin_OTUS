60fcd2ceb71981538f4cd2863740829e3b25ba76
Shkipper86 <Shkipper.r@yandex.ru>
2020-11-10 22:17:49 +0300

    Update go-pg.component.ts

--- Angular/app/my-app/src/go-pg/go-pg.component.ts
+++ Angular/app/my-app/src/go-pg/go-pg.component.ts
@@ -6,10 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./go-pg.component.css']
 })
 export class GoPgComponent implements OnInit {
- words = "Example translate";
+  words:string = '';
+  Nb: string;
+  i: number;
   constructor() { }
-
   ngOnInit(): void {
+   this.Nb=localStorage.getItem("_Words");
   }
-
+  getWords(qa: number){
+    for (let j: number=1; j <= qa; j++){
+      this.randomInteger(0,localStorage.length-4);
+      this.words=localStorage.key(this.i);
+      document.getElementById(`${j}`).innerText=`${this.words}`;
+    }
+  }
+  randomInteger(min: number, max: number){
+    let rand = min + Math.random() * (max+1);
+    this.i = Math.floor(rand);
+    return this.i;
+  }
+  checkWords(qa: number){
+    for (let j: number=1; j <= qa; j++){
+      if (localStorage.getItem(document.getElementById(`${j}`).innerText) == document.querySelectorAll("input")[j-1].value){
+        document.getElementById(`result${j}`).innerText="  True";
+      }else{
+        document.getElementById(`result${j}`).innerText="  False";
+      }
+    }
+  }
 }
