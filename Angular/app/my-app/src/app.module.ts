f1ee362b3e1ebf4138d756d6972509c17b288d8a
Shkipper86 <Shkipper.r@yandex.ru>
2020-11-10 22:17:34 +0300

    Update app.module.ts

--- Angular/app/my-app/src/app.module.ts
+++ Angular/app/my-app/src/app.module.ts
@@ -1,40 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
-import { ShkipComponent } from './shkip/shkip.component';
 import { ShkipPipePipe } from './shkip-pipe.pipe';
 import { ShkipDirectivDirective } from './shkip-directiv.directive';
-import { GameComponent } from './game/game.component';
-import { EmitComponent } from './emit/emit.component';
 import { TabsComponent } from './tabs/tabs.component';
 import { AddedPgComponent } from './added-pg/added-pg.component';
 import { SettingsPgComponent } from './settings-pg/settings-pg.component';
 import { GoPgComponent } from './go-pg/go-pg.component';
 import {Routes, RouterModule} from '@angular/router';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { TranslatePgComponent } from './translate-pg/translate-pg.component';
+import { HttpClientModule} from '@angular/common/http';
+
 
 const appRoutes: Routes =[
   {path: 'added', component: AddedPgComponent},
   {path: 'settings', component: SettingsPgComponent},
-  {path: 'go', component: GoPgComponent}
+  {path: 'go', component: GoPgComponent},
+  {path: 'translate', component: TranslatePgComponent}
 ]
 
 @NgModule({
   declarations: [
     AppComponent,
-    ShkipComponent,
     ShkipPipePipe,
     ShkipDirectivDirective,
-    GameComponent,
-    EmitComponent,
     TabsComponent,
     AddedPgComponent,
     SettingsPgComponent,
     GoPgComponent,
+    TranslatePgComponent,
   ],
   imports: [
-    BrowserModule,
-    RouterModule.forRoot(appRoutes)
+    BrowserModule.withServerTransition({ appId: 'your App-ID' }),
+    RouterModule.forRoot(appRoutes),
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
