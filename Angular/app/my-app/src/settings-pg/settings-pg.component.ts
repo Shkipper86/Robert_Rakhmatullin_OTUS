8167152acdf1b285dd4e8c5a49518d839d19c1eb
Shkipper86 <Shkipper.r@yandex.ru>
2020-11-10 22:18:10 +0300

    Update settings-pg.component.ts

--- Angular/app/my-app/src/settings-pg/settings-pg.component.ts
+++ Angular/app/my-app/src/settings-pg/settings-pg.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { from } from 'rxjs';
+
 
 @Component({
   selector: 'app-settings-pg',
@@ -6,10 +9,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./settings-pg.component.css']
 })
 export class SettingsPgComponent implements OnInit {
-
+_Lang = new FormControl('');
+_Words = new FormControl('');
   constructor() { }
 
   ngOnInit(): void {
+   this._Lang.setValue(localStorage.getItem("_Lang"));
+   this._Words.setValue(localStorage.getItem("_Words"));
+  }
+  mySetItem(){
+    localStorage.setItem("_Lang",this._Lang.value);
+    localStorage.setItem("_Words",this._Words.value);
   }
 
 }
