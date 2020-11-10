07d00519b6df665bf74e8ba24a3ffd38136a08a7
Shkipper86 <Shkipper.r@yandex.ru>
2020-11-10 22:17:31 +0300

    Update app.component.ts

--- Angular/app/my-app/src/app.component.ts
+++ Angular/app/my-app/src/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {ShkipServiceService} from './shkip-service.service';
+import {SaveLocalStorageService} from './save-local-storage.service';
 
 @Component({
   selector: 'app-root',
@@ -14,11 +15,13 @@ export class AppComponent {
   lognumReceived(s: number){
     console.log ('received', s)
   }
-  constructor(private shkipServiceService: ShkipServiceService) { }
-  ngOnInit(): void {
-    this.shkipServiceService
-    .get('USA')
-    .then(console.log)
+  //constructor(private shkipServiceService: ShkipServiceService) { }
+  //ngOnInit(): void {
+  //  this.shkipServiceService
+   // .get('USA')
+   // .then(console.log)
+  //}
+  constructor(private saveLocalStorageService: SaveLocalStorageService) { }
+  ngOninit(): void{
   }
-
 }
