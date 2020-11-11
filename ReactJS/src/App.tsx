5ae802ccdeb33fc73722195a6f6afef93b7aea80
Shkipper86 <Shkipper.r@yandex.ru>
2020-11-11 23:53:30 +0300

    Create App.tsx

---
+++ ReactJS/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import City from './components/cities';
+import { Weather } from './components/weather';
+
+const App: React.FC = () => {
+  return <>
+     <City />
+     <Weather />
+</>
+}
+
+export default App;
