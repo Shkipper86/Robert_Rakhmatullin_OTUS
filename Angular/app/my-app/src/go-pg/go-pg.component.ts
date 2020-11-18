a6d66833c3ea4bc8d6ba31deb629e033af477517
Shkipper86 <Shkipper.r@yandex.ru>
2020-11-18 23:39:44 +0300

    Update go-pg.component.ts

--- Angular/app/my-app/src/go-pg/go-pg.component.ts
+++ Angular/app/my-app/src/go-pg/go-pg.component.ts
@@ -1,37 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import {LocalStorageService} from '../local-storage.service'
 
 @Component({
   selector: 'app-go-pg',
   templateUrl: './go-pg.component.html',
   styleUrls: ['./go-pg.component.css']
 })
+
 export class GoPgComponent implements OnInit {
   words:string = '';
   Nb: string;
   i: number;
-  constructor() { }
+
+  constructor(private localStorageService: LocalStorageService) { }
+  
   ngOnInit(): void {
-   this.Nb=localStorage.getItem("_Words");
+   this.Nb= this.localStorageService.getItem("_Words");
   }
+
   getWords(qa: number){
     for (let j: number=1; j <= qa; j++){
-      this.randomInteger(0,localStorage.length-4);
-      this.words=localStorage.key(this.i);
+      this.randomInteger(0,this.localStorageService.getLenth()-4);
+      this.words = this.localStorageService.getKey(this.i);      
       document.getElementById(`${j}`).innerText=`${this.words}`;
     }
   }
+
   randomInteger(min: number, max: number){
     let rand = min + Math.random() * (max+1);
     this.i = Math.floor(rand);
     return this.i;
   }
+  
   checkWords(qa: number){
-    for (let j: number=1; j <= qa; j++){
-      if (localStorage.getItem(document.getElementById(`${j}`).innerText) == document.querySelectorAll("input")[j-1].value){
+    for (let j: number=1; j <= qa; j++){      
+      if (this.localStorageService.getItem(document.getElementById(`${j}`).innerText) == document.querySelectorAll("input")[j-1].value){
         document.getElementById(`result${j}`).innerText="  True";
       }else{
         document.getElementById(`result${j}`).innerText="  False";
       }
+
     }
   }
 }
