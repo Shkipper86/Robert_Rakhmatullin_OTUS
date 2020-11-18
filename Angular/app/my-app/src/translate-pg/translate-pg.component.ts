838431f53b73a6a121ac57fbe0faa6c83ca5d871
Shkipper86 <Shkipper.r@yandex.ru>
2020-11-18 23:40:15 +0300

    Update translate-pg.component.ts

--- Angular/app/my-app/src/translate-pg/translate-pg.component.ts
+++ Angular/app/my-app/src/translate-pg/translate-pg.component.ts
@@ -1,47 +1,51 @@
-import {Input, Component, OnInit } from '@angular/core';
-import {TranslateWordsService} from '../translate-words.service';
-import { FormControl } from '@angular/forms';
-import {AddedPgComponent} from '../added-pg/added-pg.component';
-import { analyzeAndValidateNgModules } from '@angular/compiler';
-import { async } from 'rxjs';
+import { Component, OnInit } from '@angular/core'
+import {TranslateWordsService} from '../translate-words.service'
+import { FormControl } from '@angular/forms'
+import { LocalStorageService } from '../local-storage.service'
 
 @Component({
   selector: 'app-translate-pg',
   templateUrl: './translate-pg.component.html',
   styleUrls: ['./translate-pg.component.css']
 })
+
 export class TranslatePgComponent implements OnInit {
   translateword = new FormControl('');
   wordsss$: any;
   addition: any; 
   lang: string;
-  str: any;
-  strTranslatedWord: any;
-  constructor(private translateWordsService: TranslateWordsService) { }
-  ngOnInit(): void { }
+  str: any;  
+  translatedString: string
+
+  constructor(private translateWordsService: TranslateWordsService, 
+              private localStorageService: LocalStorageService) { }
+
+  ngOnInit(): void { }  
   translate(){
     this.addition = document.getElementById('inputStr');
     this.langString();
     this.wordsss$ = this.translateWordsService.get(this.addition.value, this.lang);
     this.addVocabulary();
   }
+
   addVocabulary(){
     let strArray: string [];
     let i: number;    
     strArray = this.addition.value.split(' ');
     for (i = 0; i < strArray.length; i++){
       this.str=strArray[i];      
-      this.translateAddVocabulary(this.str);    
-      localStorage.setItem(strArray[i],this.str);      
+      this.translateAddVocabulary(this.str, this.translatedString);              
     }    
-    console.log(strArray);
   }
-    translateAddVocabulary(str){
-    this.str = this.translateWordsService.get(str, this.lang);  
-    return this.str;
+
+    async translateAddVocabulary(str, translatedString){
+    translatedString = await this.translateWordsService.get(str, this.lang);  
+    this.localStorageService.setItem(this.str, translatedString);  
+    this.str = translatedString;
   }
+  
   langString(){
-    this.lang = localStorage.getItem('_Lang');
+    this.lang = this.localStorageService.getItem('_Lang');
     switch (this.lang) {
      case "English": {
       this.lang="en";    
