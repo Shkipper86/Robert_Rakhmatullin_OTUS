4c4eec986d23638c812f56d681f1ba4318dcb61b
Shkipper86 <Shkipper.r@yandex.ru>
2020-11-18 23:39:21 +0300

    Update added-pg.component.ts

--- Angular/app/my-app/src/added-pg/added-pg.component.ts
+++ Angular/app/my-app/src/added-pg/added-pg.component.ts
@@ -1,19 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { LocalStorageService } from '../local-storage.service'
 
 @Component({
   selector: 'app-added-pg',
   templateUrl: './added-pg.component.html',
   styleUrls: ['./added-pg.component.css']
 })
+
 export class AddedPgComponent implements OnInit {
-  constructor() { }
+  constructor(private localStorageService: LocalStorageService) { }
+    output: any = '';
+    KeyValue: string;
 
   ngOnInit(): void {
-    let output: string = '';
-    for (let i: number = 0; i < localStorage.length-3; i++){
-      output=`${output}\n${localStorage.key(i)}: ${localStorage.getItem(localStorage.key(i))}\n`;
-    }
-    document.querySelector("textarea").value=output;
+        for (let i: number = 0; i < this.localStorageService.getLenth(); i++){
+          this.KeyValue = this.localStorageService.getKey(i);
+          if (this.KeyValue != "_Lang" && this.KeyValue != "_Words"){
+            this.output=`${this.output}\n${this.localStorageService.getKey(i)}: ${this.localStorageService.getItem(this.localStorageService.getKey(i))}\n`;
+          }      
+    }  
+  
   }
-
 }
