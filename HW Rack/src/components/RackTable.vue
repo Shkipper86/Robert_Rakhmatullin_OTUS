6769963c5e889187197817c56e24bcb524572242
Shkipper86 <Shkipper.r@yandex.ru>
2021-01-20 22:40:32 +0300

    Create RackTable.vue

---
+++ HW Rack/src/components/RackTable.vue
@@ -0,0 +1,26 @@
+<template>
+    <table class="table table-hover table-sm">
+          <thead>
+            <tr class="table-dark">
+              <th class="col-1">Unite</th>
+              <th class="col-5">Device</th>
+              <th class="col-1">Power</th>
+              <th class="col-1">Weight</th>
+              <th class="col-1">Status</th>
+            </tr>
+          </thead>
+          <TableRow />
+        </table>
+</template>
+
+<script>
+import TableRow from '../components/TableRow'
+export default {
+  components:{
+    TableRow
+  },
+  methods:{
+
+  }
+}
+</script>
