7cd530f86a96b52e6d5d7d7707cc657a05ccf99d
Shkipper86 <Shkipper.r@yandex.ru>
2021-01-20 22:41:00 +0300

    Create DeviceInfoHead.vue

---
+++ HW Rack/src/components/DeviceInfoHead.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h2 class="text">{{selectedRow != 'no'? unite[selectedRow].DeviceName:'no device'}}</h2>
+    </div>
+</template>
+<script>
+import {mapState} from 'vuex' 
+export default {
+    computed: mapState(['unite', 'selectedRow'])
+}
+</script>
+<style>
+.text{
+    text-align: center;
+}
+</style>
