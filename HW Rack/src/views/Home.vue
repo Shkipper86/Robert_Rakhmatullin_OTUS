90d4b2ead10b3240e5d64ecdce1573ba603e34de
Shkipper86 <Shkipper.r@yandex.ru>
2021-01-20 22:40:14 +0300

    Create Home.vue

---
+++ HW Rack/src/views/Home.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container pt-5">
+    <div class="row">
+    </div>
+    <div class="row">
+      <div class="col-md-6">
+        <div class="form-group">
+          <multiselect placeholder="Select rack" :value="value" :options="options" :searchable="false" @input="updateValueAction" ></multiselect>
+        </div>
+        <hr>
+        <RackTable />
+      </div>
+      <div class="col-md-6">
+        <DeviceInfoHead />
+        <div class="col-md-12">
+        <DeviceInfo />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import RackTable from '../components/RackTable'
+import DeviceInfo from '../components/DeviceInfo'
+import DeviceInfoHead from '../components/DeviceInfoHead'
+import Multiselect from 'vue-multiselect'
+import Vuex from 'vuex'
+const { mapActions, mapState } = Vuex
+export default {
+  name: 'Home',
+  components: {
+    RackTable,
+    DeviceInfo,
+    DeviceInfoHead,
+    Multiselect
+  },
+  methods: {
+    ...mapActions(['updateValueAction'])
+  },
+  computed: {
+    ...mapState(['value', 'options'])
+  }
+}
+</script>
