b424b6e594a0e273f7f7f7457a69acc4491c455e
Shkipper86 <Shkipper.r@yandex.ru>
2021-01-20 22:50:25 +0300

    Update main.js

--- HW Rack/src/main.js
+++ HW Rack/src/main.js
@@ -9,13 +9,13 @@ import 'firebase/database'
 Vue.config.productionTip = false
 
 firebase.initializeApp({
-  apiKey: "AIzaSyBdZ0qFycepneSVZF3N-V11vsMdSFlX5Yo",
-  authDomain: "vue-crm-fb81f.firebaseapp.com",
-  databaseURL: "https://vue-crm-fb81f-default-rtdb.firebaseio.com",
-  projectId: "vue-crm-fb81f",
-  storageBucket: "vue-crm-fb81f.appspot.com",
-  messagingSenderId: "847606494569",
-  appId: "1:847606494569:web:706a222f4db0277361f9f8"
+  apiKey: "key",
+  authDomain: "...firebaseapp.com",
+  databaseURL: "url",
+  projectId: "id",
+  storageBucket: "..appspot.com",
+  messagingSenderId: "....",
+  appId: "..."
 });
 
 new Vue({
